307ec8f25bd60f53fd323ad77a416b87d250c4a9
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-08-25 16:26:24 -0300

    Added: Home carousel layout

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const equalizeCarouselContainerHeights = () => {
+	const containers = document.querySelectorAll('[carousel-container]');
+	let prevMaxHeight = 0;
+	containers.forEach((container) => {
+		container.style.height = null;
+		container.parentElement.parentElement.style.display = 'block';
+		if (container.offsetHeight > prevMaxHeight) prevMaxHeight = container.offsetHeight;
+		container.parentElement.parentElement.style.display = null;
+	})
+	containers.forEach((container) => container.style.height = `${prevMaxHeight}px`);
+}
+
+document.addEventListener('DOMContentLoaded', equalizeCarouselContainerHeights)
+window.addEventListener('resize', equalizeCarouselContainerHeights);
