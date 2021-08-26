ea825e07bc4d29003bf147b2f7a37798e9260438
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-08-26 20:10:54 -0300

    Refactor: equalizeHeights functions

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
+const $containers = document.querySelectorAll('[carousel-container]');
 const equalizeCarouselContainerHeights = () => {
-	const containers = document.querySelectorAll('[carousel-container]');
 	let prevMaxHeight = 0;
-	containers.forEach((container) => {
+	$containers.forEach((container) => {
 		container.style.height = null;
 		container.parentElement.parentElement.style.display = 'block';
 		if (container.offsetHeight > prevMaxHeight) prevMaxHeight = container.offsetHeight;
 		container.parentElement.parentElement.style.display = null;
 	})
-	containers.forEach((container) => container.style.height = `${prevMaxHeight}px`);
+	$containers.forEach((container) => container.style.height = `${prevMaxHeight}px`);
 }
 
-equalizeCarouselContainerHeights();
-window.addEventListener('resize', equalizeCarouselContainerHeights);
+
+
+if ($containers) {
+	equalizeCarouselContainerHeights();
+	window.addEventListener('resize', equalizeCarouselContainerHeights);
+}
