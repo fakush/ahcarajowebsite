a6e4fe543fdf89634a86ce28162f87e25205f71e
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-08-26 22:18:22 -0300

    Added: Footer layout

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// --------------Initialization--------------
 const $containers = document.querySelectorAll('[carousel-container]');
 const equalizeCarouselContainerHeights = () => {
 	let prevMaxHeight = 0;
@@ -10,8 +11,29 @@ const equalizeCarouselContainerHeights = () => {
 	$containers.forEach((container) => container.style.height = `${prevMaxHeight}px`);
 }
 
+let previousPageY = 0;
+const $footer = document.querySelector('.footer');
+const hideFooterOnScroll = () => {
+	if (previousPageY < window.pageYOffset) {
+		$footer.classList.add('footer--scrolling-down')
+	} else {
+		$footer.classList.remove('footer--scrolling-down')
+	}
+	previousPageY = window.pageYOffset;
+}
+const disableHideFooterInDesktop = () => {
+	if ($footer) {
+		if (window.matchMedia('(min-width: 992px)').matches) {
+			window.removeEventListener('scroll', hideFooterOnScroll);
+		} else {
+			window.addEventListener('scroll', hideFooterOnScroll);
+		}
+	}
+}
 
-
+// --------------Execution--------------
+disableHideFooterInDesktop();
+window.addEventListener('resize', disableHideFooterInDesktop);
 if ($containers) {
 	equalizeCarouselContainerHeights();
 	window.addEventListener('resize', equalizeCarouselContainerHeights);
