9c5474d4b92711934a5450976efb85757180dd02
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-09-01 15:59:51 -0300

    Changed: Logo animation on scroll down

--- js/index.js
+++ js/index.js
@@ -1,27 +1,14 @@
 // --------------Initialization--------------
-const $containers = document.querySelectorAll('[carousel-container]');
-const equalizeCarouselContainerHeights = () => {
-	let prevMaxHeight = 0;
-	$containers.forEach((container) => {
-		container.style.height = null;
-		container.parentElement.parentElement.style.display = 'block';
-		if (container.offsetHeight > prevMaxHeight) prevMaxHeight = container.offsetHeight;
-		container.parentElement.parentElement.style.display = null;
-	})
-	$containers.forEach((container) => container.style.height = `${prevMaxHeight}px`);
-}
-
-let previousPageY = 0;
+const toRunOnScrollDown = [];
+const toRunOnScrollUp = [];
 const $footer = document.querySelector('.footer');
-const hideFooterOnScroll = () => {
-	if (previousPageY < window.pageYOffset) {
-		$footer.classList.add('footer--scrolling-down')
-	} else {
-		$footer.classList.remove('footer--scrolling-down')
-	}
-	previousPageY = window.pageYOffset;
+const $logo = document.querySelector('.header__logo');
+if ($logo) {
+	toRunOnScrollDown.push(() => $logo.classList.add('header__logo--scrolling-down'));
+	toRunOnScrollUp.push(() => $logo.classList.remove('header__logo--scrolling-down'));
 }
 
+// -------Nav underline animation-------
 const $navLinks = document.querySelectorAll(".header__nav-link");
 const $activeNavLink = document.querySelector(".header__nav-link--active");
 const $underline = document.querySelector(".header__underline");
@@ -33,9 +20,8 @@ const moveUnderlineTo = (element) => {
 		}
 	});
 	element.setAttribute("underline-target", "");
-	const styles = getComputedStyle(element);
-		
-	const contentWidth = element.getBoundingClientRect().width - parseFloat(styles.paddingRight);
+	const { paddingLeft, paddingRight } = getComputedStyle(element);
+	const contentWidth = element.getBoundingClientRect().width - parseFloat(paddingRight) - parseFloat(paddingLeft);
 	const left = element.getBoundingClientRect().left;
 	const top = element.getBoundingClientRect().top + element.getBoundingClientRect().height + underlineGap;
 	$underline.style.width = `${contentWidth}px`;
@@ -50,15 +36,42 @@ const activateThisLink = (e) => {
 	$activeNavLink.classList.remove("header__nav-link--active");
 };
 
+// -------Carousel containers with same height-------
+const $containers = document.querySelectorAll('[carousel-container]');
+const equalizeCarouselContainerHeights = () => {
+	let prevMaxHeight = 0;
+	$containers.forEach((container) => {
+		container.style.height = null;
+		container.parentElement.parentElement.style.display = 'block';
+		if (container.offsetHeight > prevMaxHeight) prevMaxHeight = container.offsetHeight;
+		container.parentElement.parentElement.style.display = null;
+	})
+	$containers.forEach((container) => container.style.height = `${prevMaxHeight}px`);
+}
+
+// -------On scroll down/up-------
+let previousPageY = 0;
+const handleScroll = () => {
+	const isScrollingDown = previousPageY < window.pageYOffset;
+	if (isScrollingDown) {
+		toRunOnScrollDown.forEach((toRun) => toRun());
+	} else {
+		toRunOnScrollUp.forEach((toRun) => toRun());
+	}
+	previousPageY = window.pageYOffset;
+}
+
+// -------Adapt to mobile/tablet/desktop-------
 const adaptToDeviceSize = () => {
 	if (window.matchMedia('(min-width: 992px)').matches) { // Desktop and above
-		if ($footer) window.removeEventListener('scroll', hideFooterOnScroll);
 		underlineGap = 12;
 	} else { // Tablets and below
-		if ($footer) window.addEventListener('scroll', hideFooterOnScroll);	
 		underlineGap = 8;
+		if ($footer) {
+			toRunOnScrollDown.push(() => $footer.classList.add('footer--scrolling-down'));
+			toRunOnScrollUp.push(() => $footer.classList.remove('footer--scrolling-down'));
+		}
 	}
-	moveUnderlineTo($activeNavLink);
 }
 
 
@@ -67,9 +80,10 @@ AOS.init({
 	once: true,
 	duration: 800
 });
-adaptToDeviceSize();
 
+adaptToDeviceSize();
 window.addEventListener('resize', adaptToDeviceSize);
+window.addEventListener('scroll', handleScroll);
 if ($containers) {
 	equalizeCarouselContainerHeights();
 	window.addEventListener('resize', equalizeCarouselContainerHeights);
@@ -79,4 +93,5 @@ if ($navLinks && $underline) {
 	$navLinks.forEach((link) => link.addEventListener("mouseenter", (e) => moveUnderlineTo(e.target)));
 	$navLinks.forEach((link) => link.addEventListener("mouseleave", underlineToActive));
 	$navLinks.forEach((link) => link.addEventListener("click", activateThisLink));
+	window.addEventListener('resize', underlineToActive);
 }
