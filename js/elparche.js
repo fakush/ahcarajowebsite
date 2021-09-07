08fc11513a326aa6867c73b46ba54d8db71dd0c4
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-09-06 21:20:37 -0300

    Added: El Parche appear animation

--- js/elparche.js
+++ js/elparche.js
@@ -1,11 +1,10 @@
-const textoTitulo = document.getElementById('tituloParche');
-const textoCuerpo = document.getElementById('textoParche');
+const $elParcheContent = document.getElementById('elParcheContent');
 
-const fillParche = (data) => {
-  const setTitulo = data[0].titulo;
-  const setCuerpo = data[0].cuerpo;
-  textoTitulo.innerHTML = setTitulo;
-  textoCuerpo.innerHTML = setCuerpo;
+const fillParche = ({ titulo, cuerpo }) => {
+	$elParcheContent.innerHTML = `
+    <h2 class="banner__title" data-aos="fade-up">${titulo}</h2>
+	  <p class="banner__text" data-aos="fade-up" data-aos-delay="100">${cuerpo}</p>
+	`
 };
 
 window.onload = function () {
