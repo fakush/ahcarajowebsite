0dfad20f948c6a99d102188e0166bca5d309dcfe
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-09-10 19:47:14 -0300

    Improved historias appear animation

--- js/frases.js
+++ js/frases.js
@@ -4,8 +4,11 @@ const createHistoriaContent = (storyList) => {
   let randomStory = Math.floor(Math.random() * storyList.length);
   const { titulo, textoNegrita, texto, autor, ubicacion } = storyList[randomStory];
   return `
-    <h2 class="phrases-history__title" data-aos="fade-up" data-aos-delay="50">${titulo}</h2>
-    <figure data-aos="fade-up" data-aos-delay="100">
+    <h2 class="phrases-history__title" data-aos="fade-up" data-aos-delay="50"
+      data-aos-anchor-placement=".banner__title">
+      ${titulo}
+    </h2>
+    <figure data-aos="fade-up" data-aos-delay="100" data-aos-anchor-placement=".banner__title">
       <blockquote class="phrases-history__blockquote">
         <span class="phrases-history__large-text">${textoNegrita}</span>
         ${texto}
